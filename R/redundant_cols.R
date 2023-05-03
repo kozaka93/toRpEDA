7712da2113b8379709620e2a71b4f593a61fff36
szuvarska <01171269@pw.edu.pl>
2023-05-03 19:15:59 +0200

    Modify redundant_cols function

--- R/redundant_cols.R
+++ R/redundant_cols.R
@@ -1,15 +1,16 @@
 #' redundant_cols`()` suggests redundant columns.
-#' 
+#'
 #' @param df Data frame.
 #' @param variables Vector of names of variables to consider. All of variables
 #' are taken as default.
-#' @param delete If `TRUE`, function returns data frame with deleted redundant 
+#' @param delete If `TRUE`, function returns data frame with deleted redundant
 #' columns. `FALSE` as default.
 #' @param  correlated If `TRUE`, function treats highly correlated variables as redundant columns and also deletes
 #' them if `delete` is `TRUE`. `FALSE` as default.
 #'
 #' @return If `delete` = `FALSE`, vector of names of redundant columns. Otherwise
-#' data frame with deleted redundant columns.
+#' data frame with deleted redundant columns. In case there are not any redundant
+#' columnm, `character(0)` is returned.
 #'
 #' @examples
 #' library("toRpEDA")
@@ -21,33 +22,47 @@
 redundant_cols <- function(df, variables = colnames(df), correlated = FALSE, delete = FALSE) {
   if(!is.data.frame(df))
     stop("df is not a data frame!")
+
   if(!is.character(variables))
     stop("variables is not a character vector!")
+  else if(!all(variables %in% colnames(df))) {
+    message("chosen variables do not exist in dataframe, all variables will be taken")
+    variables = colnames(df)
+  }
+
   if(!is.logical(delete))
     stop("delete is not logical")
   if(!is.logical(correlated))
     stop("correlated is not logical")
-  if(!all(variables %in% colnames(df))) {
-    message("chosen variables do not exist in dataframe, all variables will be taken")
-    variables = colnames(df)
-  }
+
   if(NROW(df) == 0)
     message("df is empty")
+
   df <- dplyr::select(df, dplyr::all_of(variables))
-  cols <- c()
+
+  duplicated_cols <- duplicated(as.list(df))
+  variables <- variables[!duplicated_cols]
+  cols <- colnames(df[duplicated_cols])
+
+  df <- dplyr::select(df, dplyr::all_of(variables))
+
   n <- NROW(df)
+
   for(col in variables) {
     n_unique <- dplyr::n_distinct(df[,col], na.rm = TRUE)
-    if(n_unique == n | n_unique == 1 )
+    if(n_unique == n | n_unique == 1 ) {
       cols <- c(cols, col)
+      df <- select(df, -col)
+    }
   }
   if(correlated) {
     cor_matrix <- stats::cor(dplyr::select_if(df, is.numeric))
     highly_correlated <- which(abs(cor_matrix) > 0.9 & upper.tri(cor_matrix), arr.ind = TRUE)
     highly_correlated <- unique(unlist(dimnames(highly_correlated)[1]))
     cols <- c(cols, highly_correlated)
+    df <- dplyr::select(df, -one_of(highly_correlated))
   }
   if(delete)
-    return(df[, !(colnames(df) %in% cols)])
+    return(df)
   return(cols)
 }
--- tests/testthat/test-redundant_cols.R
+++ tests/testthat/test-redundant_cols.R
@@ -14,12 +14,13 @@ test_that("result is a character vector", {
 })
 
 test_that("result is null", {
-  expect_null(redundant_cols(iris))
+  expect_equal(redundant_cols(iris), character(0))
 })
 
 test_that("errors work", {
   expect_error(redundant_cols(c(1,2,3)))
   expect_error(redundant_cols(iris, variables = c(1,2,3)))
+  expect_error(redundant_cols(iris, variables = c()))
   expect_error(redundant_cols(iris, delete = "Yes"))
   expect_error(redundant_cols(iris, correlated = "Yes"))
 })
@@ -27,6 +28,7 @@ test_that("errors work", {
 test_that("messages work", {
   expect_message(redundant_cols(data.frame()))
   expect_message(redundant_cols(iris, variables = "variable"))
+  expect_message(redundant_cols(iris, variables = ""))
 })
 
 test_that("result is a list when delete = TRUE", {
@@ -38,11 +40,17 @@ test_that("function works with only one variable", {
   df$index <- 1:NROW(df)
   df$static <- 5
   expect_equal(redundant_cols(df, variables = "index"), "index")
-  expect_null(redundant_cols(iris, variables = "Sepal.Length"))
+  expect_equal(redundant_cols(iris, variables = "Sepal.Length"), character(0))
 })
 
 test_that("finding highly correlated columns works", {
   df <- mtcars
-  mtcars$variable <- mtcars$hp * 5
-  expect_equal(redundant_cols(mtcars, correlated = TRUE), c("cyl", "hp"))
+  df$variable <- df$hp * 5
+  expect_equal(redundant_cols(df, correlated = TRUE), c("cyl", "hp"))
+})
+
+test_that("finding duplicate columns works", {
+  df <- iris
+  df$something <- df$Sepal.Length
+  expect_equal(redundant_cols(df), "something")
 })
