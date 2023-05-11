881689a81a8b6b0642a3761bc1507327c360b450
Szymon-Gut <72514490+Szymon-Gut@users.noreply.github.com>
2023-05-11 17:18:31 +0200

    funcionalities added

---
+++ R/plot_histogram.R
@@ -0,0 +1,60 @@
+library(ggplot2)
+library(patchwork)
+
+
+plot_hist_vars <- function(data, vars, plots_per_page = 6) {
+  all_numeric_vars_idx <- sapply(data, is.numeric)
+
+  if (!any(all_numeric_vars_idx)) {
+    warning("No numeric variables found.")
+    return(NULL)
+  }
+
+  if (missing(vars)) {
+    data_numeric <- data[, all_numeric_vars_idx]
+
+  } else {
+
+    all_data_by_user_idx <- names(data) %in% vars
+
+    data_by_user <- data[, all_data_by_user_idx]
+
+    numeric_vars_by_user_idx <- all_numeric_vars_idx & all_data_by_user_idx
+
+    check_if_numeric_user_data <- sapply(data_by_user, is.numeric)
+
+    if (!all(check_if_numeric_user_data)) {
+      warning(paste("Following columns are not numeric:", paste(names(data_by_user)[!check_if_numeric_user_data], collapse = ", ")))
+    }
+
+    data_numeric <- data[, numeric_vars_by_user_idx]
+  }
+
+  plots <- vector("list", ncol(data_numeric))
+
+  for (i in 1:ncol(data_numeric)) {
+      plot <-  ggplot(data = data_numeric, aes_string(x = colnames(data_numeric)[i])) +
+               geom_histogram(fill = "#F8766D", alpha = 0.8) +
+               labs(title = colnames(data_numeric)[i], x = "Value", y = "Frequency") +
+               ggtitle(paste("Histogram of", colnames(data_numeric)[i])) +
+               xlab(colnames(data_numeric)[i])
+      plots[[i]] <- plot
+  }
+
+  n_pages <- ceiling(length(plots) / plots_per_page)
+
+  for (i in seq_len(n_pages)) {
+    start_idx <- (i - 1) * plots_per_page + 1
+    end_idx <- min(i * plots_per_page, length(plots))
+    page_plots <- plots[start_idx:end_idx]
+    page <- wrap_plots(page_plots, nrow = 2, ncol = 3)
+    print(page)
+  }
+}
+
+
+
+data(mtcars)
+plot_hist_vars(mtcars, c("mpg", "cyl"))
+plot_hist_vars(iris, c("Sepal.Length", "Petal.Length", "Species"))
+
