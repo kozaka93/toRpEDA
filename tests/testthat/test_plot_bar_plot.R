db0d1fe85bd4d97ff44614fa83b74802a92bc589
D3TR0 <32223800+D3TR0@users.noreply.github.com>
2023-05-31 21:23:43 +0200

    Add one more test

--- tests/testthat/test_plot_bar_plot.R
+++ tests/testthat/test_plot_bar_plot.R
@@ -17,3 +17,16 @@ test_that("check arguments",{
   expect_error(plot_bar_plot(my_data, "target", variables = c("a", "d")))
   expect_error(plot_bar_plot(my_data[1:2], "target"))
 })
+
+test_that("function works", {
+  my_data <- data.frame(
+    target = c("R", "Python", "R", "R", "Python", "Python"),
+    a = c(1, 2, 3, 4, 2, 2),
+    b = c("a", "b", "c", "b", "c", "c"),
+    c = c(TRUE, FALSE, TRUE, FALSE, FALSE, TRUE),
+    d = c("dog ", "cat", "dog", "dog", "dog", "cat"),
+    e = c("PL", "PL", "PL", "USA", "USA", "USA"),
+    f = c("Europa", "Europa", "America", "Europa", "America", "Europa")
+  )
+  expect_type(plot_bar_plot(my_data, "target"), "list")
+})
