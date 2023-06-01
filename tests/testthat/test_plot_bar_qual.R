802d55b74748deb2b6c52dd9f9f4d0b9576b6cde
grzegorzpioro <72441347+grzegorzpioro@users.noreply.github.com>
2023-06-01 17:22:38 +0200

    Update test_plot_bar_qual.R

--- tests/testthat/test_plot_bar_qual.R
+++ tests/testthat/test_plot_bar_qual.R
@@ -1,13 +1,13 @@
-test_that("Czy wartosc korelacji pearsona jest ok",
+test_that("Does it return a NULL",
           {expect_null(plot_bar_qual(iris))})
 
-test_that("Czy numeryczne wyrzuca error",
+test_that("Numeric return error",
           {expect_error(plot_bar_qual(iris,c(1)))})
 
-test_that("Czy numeryczne wyrzuca error",
+test_that("Numeric return error",
           {expect_error(plot_bar_qual(mtcars,c(1,3)))})
 
-test_that("Czy dziala dla stringow, jedna kolumna",
+test_that("Does it work for one column",
           {expect_error(plot_bar_qual(iris,c(5)),NA)})
 
 
