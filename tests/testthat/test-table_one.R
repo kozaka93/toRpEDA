bd459325096defdc3e4e41bc29fd14b204c2c701
dyszkiewiczw <72323300+dyszkiewiczw@users.noreply.github.com>
2023-06-01 23:55:49 +0200

    Add files via upload

---
+++ tests/testthat/test-table_one.R
@@ -0,0 +1,27 @@
+
+test_that('errors',{
+  expect_error(table_one(numeric_vars = 'Species',data = iris))
+  expect_error(table_one(groupby = 'Species',data = iris))
+  expect_error(table_one(data = iris))
+  expect_error(table_one(c('Petal.Len'),groupby = 'Species',data = iris))
+  expect_error(table_one('cars', 'cars', list('cars' = c(1,2,3))))
+  expect_error(table_one(c("Petal.Length","Petal.Width"), c(1), iris))
+})
+
+
+test_that('warnings',{
+  iris_with_NA <- iris
+  iris_with_NA[,'NAs'] <- 'NA'
+  expect_warning(table_one(colnames(iris), 'Species', iris))
+  expect_warning(table_one(c("Petal.Length","Petal.Width","NAs"),
+                           groupby = "Species", data= iris_with_NA))})
+
+test_that('value',{
+  try_2 <- iris %>% group_by(Species) %>% summarise(n=n())
+  try <- (table_one(c("Petal.Length","Petal.Width"),'Species',iris))
+  expect_equal(as.numeric(try[1,1]),try_2[[1,2]] )
+})
+
+test_that('result type', {
+  expect_type(table_one(c("Petal.Length","Petal.Width"),'Species',iris), "list")
+})
