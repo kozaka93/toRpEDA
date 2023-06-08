23dbd71ed83373b18517a25e8b06ea6c7974ffa7
Arkadiusz Kniaź <56119871+arekkn@users.noreply.github.com>
2023-06-08 17:42:10 +0200

    Update impute_missing_data.R

--- R/impute_missing_data.R
+++ R/impute_missing_data.R
@@ -4,7 +4,7 @@
 #' The function can impute missing data for all columns or for a selected set of columns.
 #'
 #' @param df A data frame containing the data to impute.
-#' @param cols_list A list of character vectors specifying the columns to impute for each method.
+#' @param variables A list of character vectors specifying the columns to impute for each method.
 #'   Each element of the list should be a character vector containing the names of the columns to impute
 #'   using the corresponding method in the `methods` parameter. The default value is a list containing all column names of `df`.
 #' @param methods A character vector specifying the methods to use for imputing missing data.
@@ -18,9 +18,24 @@
 #'
 #' @export
 #'
-impute_missing_data <- function(df, cols_list=list(colnames(df)), methods=c("mean")) {
+impute_missing_data <- function(df, variables = list(colnames(df)), methods=c("mean")) {
+
+  if(!is.data.frame(df))
+    stop("df is not a data frame!")
+
+  if(!is.list(variables) || !all(sapply(variables, is.character)))
+    stop("variables is not a list of character vectors!")
+
+  if(!all(unlist(lapply(variables, function(x) all(x %in% colnames(df)))))) {
+    message("Some chosen variables do not exist in dataframe, all variables will be taken")
+    variables <- rep(list(colnames(df)), length(methods))
+  }
+
+  if (!is.character(methods) || !all(methods %in% c("mean", "median", "mode")))
+    stop("methods must be a character vector containing only 'mean', 'median' or 'mode'!")
+                        
   impute_data <- function(i) {
-    cols <- cols_list[[i]]
+    cols <- variables[[i]]
     method <- methods[i]
     if (method == "mean") {
       df <<- impute_mean(df, cols)
@@ -30,7 +45,7 @@ impute_missing_data <- function(df, cols_list=list(colnames(df)), methods=c("mea
       df <<- impute_mode(df, cols)
     }
   }
-  lapply(seq_along(cols_list), impute_data)
+  lapply(seq_along(variables), impute_data)
   return(df)
 }
 #' Impute mean
@@ -39,7 +54,7 @@ impute_missing_data <- function(df, cols_list=list(colnames(df)), methods=c("mea
 #' The function can impute mean for all columns or for a selected set of columns.
 #'
 #' @param df A data frame containing the data to impute.
-#' @param cols A character vector containing the names of the columns to impute
+#' @param variables A character vector containing the names of the columns to impute
 #'   using mean. The default value is a list containing all column names of `df`.
 #'
 #' @return A data frame with the same dimensions as `df` where missing values have been imputed using the mean.
@@ -50,14 +65,19 @@ impute_missing_data <- function(df, cols_list=list(colnames(df)), methods=c("mea
 #'
 #' @export
 #'
-impute_mean <- function(df, cols = colnames(df)) {
+impute_mean <- function(df, variables = colnames(df)) {
   impute_col_mean <- function(col) {
     if (!is.character(col) && !is.factor(col)) {
       col[is.na(col)] <- mean(col, na.rm = TRUE)
     }
+    if (inherits(col, "Date")) {
+      mean_date <- mean(col, na.rm = TRUE)
+      col[is.na(col)] <- mean_date
+      col <- as.Date(as.double(col), origin = "1970-01-01")
+    }
     return(col)
   }
-  df[cols] <- mapply(impute_col_mean, df[cols])
+  df[variables] <- lapply(df[variables], impute_col_mean)
   return(df)
 }
 #' Impute mode
@@ -66,7 +86,7 @@ impute_mean <- function(df, cols = colnames(df)) {
 #' The function can impute mode for all columns or for a selected set of columns.
 #'
 #' @param df A data frame containing the data to impute.
-#' @param cols A character vector containing the names of the columns to impute
+#' @param variables A character vector containing the names of the columns to impute
 #'   using mode. The default value is a list containing all column names of `df`.
 #'
 #' @return A data frame with the same dimensions as `df` where missing values have been imputed using the mode.
@@ -77,7 +97,7 @@ impute_mean <- function(df, cols = colnames(df)) {
 #'
 #' @export
 #'
-impute_mode <- function(df, cols = colnames(df)) {
+impute_mode <- function(df, variables = colnames(df)) {
   calc_mode <- function(x) {
     if (is.character(x) | is.logical(x)) {
       names(which.max(table(x)))
@@ -88,7 +108,7 @@ impute_mode <- function(df, cols = colnames(df)) {
       ux[which.max(tabulate(match(x, ux)))]
     }
   }
-  df[cols] <- lapply(df[cols], function(x) {
+  df[variables] <- lapply(df[variables], function(x) {
     mode_val <- calc_mode(na.omit(x))
     x[is.na(x)] <- mode_val
     x
@@ -101,7 +121,7 @@ impute_mode <- function(df, cols = colnames(df)) {
 #' The function can impute median for all columns or for a selected set of columns.
 #'
 #' @param df A data frame containing the data to impute.
-#' @param cols A character vector containing the names of the columns to impute
+#' @param variables A character vector containing the names of the columns to impute
 #'   using median. The default value is a list containing all column names of `df`.
 #'
 #' @return A data frame with the same dimensions as `df` where missing values have been imputed using the median.
@@ -112,16 +132,18 @@ impute_mode <- function(df, cols = colnames(df)) {
 #'
 #' @export
 #'
-impute_median <- function(df, cols = colnames(df)) {
+impute_median <- function(df, variables = colnames(df)) {
   impute_col_median <- function(col) {
     if (!is.character(col) && !is.factor(col) && !is.complex(col)) {
       col[is.na(col)] <- median(col, na.rm = TRUE)
     }
+    if (inherits(col, "Date")) {
+      median_date <- median(col, na.rm = TRUE)
+      col[is.na(col)] <- median_date
+      col <- as.Date(as.double(col), origin = "1970-01-01")
+    }
     return(col)
   }
-  df[cols] <- mapply(impute_col_median, df[cols])
+  df[variables] <- lapply(df[variables], impute_col_median)
   return(df)
-  }
-
-
-
+}
