1a2da6a22cc89da38d6e29fee9ecf22c32c98b45
dyszkiewiczw <72323300+dyszkiewiczw@users.noreply.github.com>
2023-06-08 16:33:32 +0200

    Update table_one.R
    pare poprawek dotyczących opisu, zmiana kolejności ifów żeby było lepsze pokrycie, dodanie ifów dot. komentarzy

--- R/table_one.R
+++ R/table_one.R
@@ -1,47 +1,45 @@
 #' Dataset Information
-#' Function generates a list of the most common statistics
+#' Function generates a data frame of the most common statistics
 #' such as mean, standard deviation, minimum and maximum
-#' grouped by selected argument
+#' grouped by selected argument for numerical variables
 #'
 #' @param data Data frame
 #' @param numeric_vars column names of numeric columns of dataframe which we want to use to have table one
 #' arguments should be typed as character vector
-#' @param groupby element which we want to group by the rest numeric variables
-#'
-
-#' @examples
-#' library("toRpEDA")
-#' library(dplyr)
-#' library(stats)
-#' table_one(numeric_vars = c("Petal.Length","Petal.Width"), groupby = "Species", data= iris)
+#' @param groupby element which we want to group by the rest numeric variables but only one grupby element
 #'
 #' @return returns a list of the most commonly used statistical measures
 #' such as mean, standard deviation, minimum and maximum
 #' of desired numerical variables
 #' @import stats
 #' @import dplyr
+#' @examples
+#' library("toRpEDA")
+#' library(dplyr)
+#' library(stats)
+#' table_one(numeric_vars = c("Petal.Length","Petal.Width"), groupby = "Species", data= iris)
+#' table_one(groupby = 'Species', data= iris)
 #' @export
-table_one <-function(numeric_vars, groupby, data){
+
+table_one <-function(numeric_vars = NULL, groupby, data){
   data[data == ''] <- NA
-  if (!all(numeric_vars %in% colnames(data))){
-    stop("Please enter valid column names")
-  }
   if (missing(data)){
     stop("Please enter valid data")
-  }
-  if (!is.data.frame(data)) {
-    stop("Please type data frame in data argument")
   }
   if (missing(groupby)) {
     stop("Please enter groupby argument")
   }
-  ## Give a class
-  #class(TableOneObject) <- "TableOne"
-  if(!is.character(groupby)){
-    stop("Please enter character groupby argument")
+  if (!all(numeric_vars %in% colnames(data))){
+    stop("Please enter valid column names")
   }
-  if(missing(numeric_vars)){
-    stop("Please enter numeric column names argument")
+  if(length(groupby)>1){
+    stop("Please enter only one groupby element")
+  }
+  if (!all(groupby %in% colnames(data))){
+    stop("Please enter valid column name for groupby argument")
+  }
+  if (!is.data.frame(data)) {
+    stop("Please type data frame in data argument")
   }
   if(!all(unlist(lapply(data[,numeric_vars], is.numeric), use.names = FALSE))){
     warning("One of variables is non-numeric, function will take
@@ -49,6 +47,9 @@ table_one <-function(numeric_vars, groupby, data){
     wh <- unlist(lapply(data[,numeric_vars], is.numeric), use.names = FALSE)
     numeric_vars <- c(colnames(data[,numeric_vars])[wh])
   }
+  if(is.null(numeric_vars)){
+    numeric_vars <- colnames(data %>% select(-all_of(c(groupby))))
+    }
   functions<- function(x){
     c(
       "mean (SD)" = paste(format(round(mean(x, na.rm = TRUE),3),nsmall=3),
@@ -65,7 +66,6 @@ table_one <-function(numeric_vars, groupby, data){
   n <- t(n)
   colnames(n) <- n[1,]
   n <- n[-1, ]
-  out <- rbind(n,output)
-  out <- as.data.frame(out)
+  out <- as.data.frame(rbind(n,output))
   return(out)
 }
