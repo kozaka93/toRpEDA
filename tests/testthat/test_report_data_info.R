f64dcc131b0be51d51797dc8b451b7a4b12af5ef
Kacper Skonieczka <kacper.skonieczka@respect.energy>
2023-06-12 18:57:32 +0200

    fix bugs

--- tests/testthat/test_report_data_info.R
+++ tests/testthat/test_report_data_info.R
@@ -2,7 +2,6 @@ test_that("report_data_info generates a report", {
   output_file <- "Test_report_data_info.html"
   output_dir = getwd()
   raport_data_info(iris, output_file = output_file)
-  raport_data_info(iris, output_file = output_file)
   expect_true(file.exists(output_file))
   expect_true(file.size(output_file) > 0)
   unlink(output_file)
