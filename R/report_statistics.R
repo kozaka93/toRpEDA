d7905174bb0e14887142a68d21efa4d84598ff3a
malwina0 <72470800+malwina0@users.noreply.github.com>
2023-06-19 20:43:34 +0200

    fix: corrected variable names etc
    Teraz wydaje mi się że test mojej funkcji powinien przechodzić

--- R/report_statistics.R
+++ R/report_statistics.R
@@ -4,7 +4,8 @@
 #'
 #' @param df A data frame for which the report is to be generated.
 #' @param variables Variables for which the report is to be generated.
-#' @param output_filename The output file name.
+#' @param output_file A string giving the name of the output file. Default is NULL.
+#' @param output_dir A string giving the directory where the output file will be saved. Default is the current working directory.
 #'
 #' @return Report is saved as local file. It contains basic descriptive statistics, 
 #' a histogram and correlation matrix for quantitative variables, and barplots 
@@ -17,15 +18,15 @@
 #' report_statistics(iris, output_file = "iris_statistics_report.html")
 #' }
 
-report <-
+report_statistics <-
   function(df,
            variables = NULL,
            output_file = NULL, 
            output_dir = getwd()) {
     
     if (is.null(variables))
-    
-    input_file_path <- system.file('rmd', 'report_statistics.Rmd', package = 'toRpEDA')
+      
+      input_file_path <- system.file('rmd/report_statistics', 'report_statistics.Rmd', package = 'toRpEDA')
     
     rmarkdown::render(
       input         = input_file_path,
